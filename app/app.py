e5be95869f9ce913c5e0bb7aeb6be375383b129e
Ananxv <anainesj@tecnico.ulisboa.pt>
2024-03-15 13:56:47 +0000

    'send by email' button added

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from flask import render_template, request, redirect, url_for
 from flask import jsonify
 from datetime import datetime, timedelta
 import mysql.connector
+import smtplib
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
@@ -14,21 +16,18 @@ DB_PORT = 3306
 DB_PASSWORD=""
 DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD)
 
+# Query
+QUERY_SELECT_PRODUCTS = "SELECT product_name, quantity, expiration_date FROM product;"
+
 products = {
     "items": [],
     "quantities": [],
     "expiration_dates":[]
 }
 
-dbConn = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_DATABASE
-)
-
 dbConn = None
 cursor = None
+
 try:
     dbConn = mysql.connector.connect(
         host=DB_HOST,
@@ -38,9 +37,7 @@ try:
     )
     cursor = dbConn.cursor()
 
-    query = ("SELECT product_name, quantity, expiration_date FROM product;")
-
-    cursor.execute(query)
+    cursor.execute(QUERY_SELECT_PRODUCTS)
     rows = cursor.fetchall()
 
     for i in range(len(rows)):
@@ -55,6 +52,43 @@ except Exception as e:
 finally:
     cursor.close()
     dbConn.close()
+    
+def send_email(subject, message):
+    # Email credentials
+    sender_email = "chipthefridge@gmail.com"
+    sender_password = "lyuv pgnu wboz ewzj"
+    recipient_email = "anainesanxv@gmail.com"
+        
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = ', '.join(recipient_email)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender_email, sender_password)
+       smtp_server.sendmail(sender_email, recipient_email, msg.as_string())
+    print("Message sent!")
+    
+def create_shopping_list():
+    shopping_list = []
+    for i in range(len(products["items"])):
+        product_name = products["items"][i]
+        quantity = products["quantities"][i]
+        
+        if quantity < 2:
+            shopping_list += [product_name]
+    
+    return shopping_list
+
+@app.route('/send_shopping_list')
+def send_shopping_list():
+    shopping_list = create_shopping_list()
+    subject = "Shopping List"
+    message = ""
+    for item in shopping_list:
+        message += "- " + item + "\n"
+    
+    send_email(subject, message)
+    return jsonify({'message': 'Shopping List sent successfully!'})
 
 @app.route('/')
 def home():
@@ -63,9 +97,8 @@ def home():
 
 @app.route('/data')
 def fetch_data():
-    shopping_list = {'Milk': 3, 'Bread':7, 'Eggs':2, 'Bananas': 7}
+    shopping_list = create_shopping_list()
     notifications = fetch_notifications()
-
     return shopping_list, notifications
         
 @app.route('/fetch_fridge_data')
@@ -80,7 +113,7 @@ def fetch_fridge_data():
         fridge_data['quantities'] += [products["quantities"][i]]
         
     return jsonify(fridge_data)
-        
+
 @app.route('/fetch_notifications')
 def fetch_notifications():
         
@@ -94,7 +127,6 @@ def fetch_notifications():
         quantity = products["quantities"][i]
         expiration_date = products["expiration_dates"][i]
         
-        # product_date = datetime.strptime(expiration_date, '%Y-%m-%d').date()
         product_date = expiration_date
         current_date = datetime.now().date()
 
