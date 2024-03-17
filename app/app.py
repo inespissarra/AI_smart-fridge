ee3ddde83fab6f63bf1d3c02c5473e40556d1dee
Ananxv <anainesj@tecnico.ulisboa.pt>
2024-03-17 16:14:18 +0000

    app changed: database operation changed

--- app/app.py
+++ app/app.py
@@ -19,76 +19,42 @@ DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (DB_HOST, DB_DA
 # Query
 QUERY_SELECT_PRODUCTS = "SELECT product_name, quantity, expiration_date FROM product;"
 
-products = {
-    "items": [],
-    "quantities": [],
-    "expiration_dates":[]
-}
+def get_products():
+    products = {
+        "items": [],
+        "quantities": [],
+        "expiration_dates":[]
+    }
+    dbConn = None
+    cursor = None
 
-dbConn = None
-cursor = None
+    try:
+        dbConn = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_DATABASE
+        )
+        cursor = dbConn.cursor()
 
-try:
-    dbConn = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_DATABASE
-    )
-    cursor = dbConn.cursor()
+        cursor.execute(QUERY_SELECT_PRODUCTS)
+        rows = cursor.fetchall()
 
-    cursor.execute(QUERY_SELECT_PRODUCTS)
-    rows = cursor.fetchall()
-
-    for i in range(len(rows)):
-        products['items'] += [rows[i][0]]
-        products['quantities'] += [rows[i][1]]
-        products['expiration_dates'] += [rows[i][2]]
-        print("There's {} of {}".format(rows[i][1], rows[i][0]))
-
-except Exception as e:
-    print("error: " + str(e))
-
-finally:
-    cursor.close()
-    dbConn.close()
-    
-def send_email(subject, message):
-    # Email credentials
-    sender_email = "chipthefridge@gmail.com"
-    sender_password = "lyuv pgnu wboz ewzj"
-    recipient_email = "anainesanxv@gmail.com"
+        for i in range(len(rows)):
+            products['items'] += [rows[i][0]]
+            products['quantities'] += [rows[i][1]]
+            products['expiration_dates'] += [rows[i][2]]
+            print("There's {} of {}".format(rows[i][1], rows[i][0]))
         
-    msg = MIMEText(message)
-    msg['Subject'] = subject
-    msg['From'] = sender_email
-    msg['To'] = ', '.join(recipient_email)
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender_email, sender_password)
-       smtp_server.sendmail(sender_email, recipient_email, msg.as_string())
-    print("Message sent!")
-    
-def create_shopping_list():
-    shopping_list = []
-    for i in range(len(products["items"])):
-        product_name = products["items"][i]
-        quantity = products["quantities"][i]
-        
-        if quantity < 2:
-            shopping_list += [product_name]
-    
-    return shopping_list
+        return products
 
-@app.route('/send_shopping_list')
-def send_shopping_list():
-    shopping_list = create_shopping_list()
-    subject = "Shopping List"
-    message = ""
-    for item in shopping_list:
-        message += "- " + item + "\n"
-    
-    send_email(subject, message)
-    return jsonify({'message': 'Shopping List sent successfully!'})
+    except Exception as e:
+        print("error: " + str(e))
+
+    finally:
+        cursor.close()
+        dbConn.close()
+
 
 @app.route('/')
 def home():
@@ -97,8 +63,9 @@ def home():
 
 @app.route('/data')
 def fetch_data():
-    shopping_list = create_shopping_list()
-    notifications = fetch_notifications()
+    products = get_products()
+    shopping_list = create_shopping_list(products)
+    notifications = fetch_notifications(products)
     return shopping_list, notifications
         
 @app.route('/fetch_fridge_data')
@@ -107,6 +74,8 @@ def fetch_fridge_data():
         "items": [],
         "quantities": []
     }
+    
+    products = get_products()
 
     for i in range(len(products["items"])):    
         fridge_data['items'] += [products["items"][i]]
@@ -115,7 +84,7 @@ def fetch_fridge_data():
     return jsonify(fridge_data)
 
 @app.route('/fetch_notifications')
-def fetch_notifications():
+def fetch_notifications(products):
         
     notifications = {
         "expiration_dates": [],
@@ -141,6 +110,43 @@ def fetch_notifications():
             print("{} {} is expired".format(quantity, product_name))
             
     return notifications
+
+def send_email(subject, message):
+    # Email credentials
+    sender_email = "chipthefridge@gmail.com"
+    sender_password = "lyuv pgnu wboz ewzj"
+    recipient_email = "anainesanxv@gmail.com"
+        
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = ', '.join(recipient_email)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender_email, sender_password)
+       smtp_server.sendmail(sender_email, recipient_email, msg.as_string())
+    print("Message sent!")
+    
+def create_shopping_list(products):
+    shopping_list = []
+    for i in range(len(products["items"])):
+        product_name = products["items"][i]
+        quantity = products["quantities"][i]
+        
+        if quantity < 2:
+            shopping_list += [product_name]
+    
+    return shopping_list
+
+@app.route('/send_shopping_list')
+def send_shopping_list():
+    shopping_list = create_shopping_list(products=get_products())
+    subject = "Shopping List"
+    message = ""
+    for item in shopping_list:
+        message += "- " + item + "\n"
+    
+    send_email(subject, message)
+    return jsonify({'message': 'Shopping List sent successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
